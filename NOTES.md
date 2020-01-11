59ac845b8f79955abdf0d9f8bfcc2ce5230b0010
Michael B. Akinlabi <35458044+MBAkinlabi@users.noreply.github.com>
2020-01-11 23:19:03 +0100

    Adding and Using Radium

--- NOTES.md
+++ NOTES.md
@@ -1,2 +1,6 @@
-# Setting Class Names Dynamically
-- How to dynamically add CSS classes to your app. You do this automatically when something happens. It's so great to be able to do this dynamically.
+# Adding and Using Radium
+- Because we can't use pseudo (button:hover) for our inline classes because we can't write it like that.
+- And we can't do it in the css file (button: dkdkd) because it'll affect all our buttons in the app, we need to install a third-party package to do this.
+```shell
+npm install --save radium
+```
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -50,7 +51,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -70,6 +75,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }; 
     }
 
     const classes = [];
@@ -93,4 +102,4 @@ class App extends Component {
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
-export default App;
+export default Radium(App);
