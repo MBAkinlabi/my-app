c24f0eaded0ab7f5df7867663c5b015ed94d9f59
Michael B. Akinlabi <35458044+MBAkinlabi@users.noreply.github.com>
2020-01-30 11:24:44 +0100

    contextType & useContext()

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,4 @@
-# Using the Context API
-- This is a great alternative to the one we did in the last lecture. 
-- If you want to pass data from A to D, this is what you should use. It's so cool instead of passing through every child component when they don't need it.
+# contextType & useContext()
+- We talked about context in the last lecture. There's an alternative and elegant way of using in class and also functional components.
+- See how instructor use `contextType` in class to make using context even more simpler
+- And see how the instructor use the React Hook`useContext()` in functional component to make using context more simple and easy. Wow. This is getting interesting.
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -12,23 +12,23 @@ class Person extends Component {
         this.inputElementRef = React.createRef();
     }
 
+    static contextType = AuthContext;
+
     componentDidMount() {
         // This is the first way
         // this.inputElement.focus();
 
         // Note that .current is selecting the current element.
         this.inputElementRef.current.focus();
+        console.log(this.context.authenticated);
     }
 
     render() {
         console.log('[Person.js] rendering...');
         return(
             <Aux>
-                <AuthContext.Consumer>
-                    {(context) => 
-                        context.authenticated ? <p>Authenticated</p> : <p>Please log in</p> 
-                    }
-                </AuthContext.Consumer>
+                {this.context.authenticated ? <p>Authenticated</p> : <p>Please log in</p>}
+                    
                 <p onClick={this.props.click}>
                     I'm {this.props.name} and I am {this.props.age} years old!
                 </p>
